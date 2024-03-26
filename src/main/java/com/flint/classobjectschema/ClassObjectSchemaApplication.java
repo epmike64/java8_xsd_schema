7c763e43cb683a132f8bf75ee0263cac0f892925
epmike64 <epmike64@gmail.com>
2024-03-26 17:58:35 -0400

    Java Grammar is taken from here https://docs.oracle.com/javase/specs/jls/se8/html/jls-19.html

---
+++ src/main/java/com/flint/classobjectschema/ClassObjectSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.flint.classobjectschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassObjectSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassObjectSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flint/classobjectschema/ClassObjectSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flint.classobjectschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassObjectSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
